26f58d64ec0e8039b952640eea3fb15c693ea718
Jan Cichosz <jan.cichosz@web.de>
2025-05-26 13:41:49 +0200

    removed some stuff from mcg_visual so i had to adjust

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ mod utils;
 
 use eframe::CreationContext;
 use egui_extras::install_image_loaders;
-use mcg_visual::game::screen::{CardsTestDND, DNDTest, Game, GameSetupScreen};
+use mcg_visual::game::screen::{Game, GameSetupScreen};
 use mcg_visual::game::App;
 use std::cell::RefCell;
 use std::rc::Rc;
@@ -29,20 +29,9 @@ pub fn start(canvas: HtmlCanvasElement) -> Result<(), JsValue> {
         let game_conf = Rc::new(RefCell::new(GameSetupScreen::new(Rc::downgrade(
             &game_widget,
         ))));
-        let dnd_test = Rc::new(RefCell::new(DNDTest::new()));
         app.register_screen(String::from("game"), game_widget)
             .unwrap();
         app.register_screen(String::from("game_setup"), game_conf)
-            .unwrap();
-        app.register_screen(String::from("dnd_test"), dnd_test)
-            .unwrap();
-        let game_dnd_widget = Rc::new(RefCell::new(CardsTestDND::new()));
-        let game_dnd_conf = Rc::new(RefCell::new(GameSetupScreen::new(Rc::downgrade(
-            &game_dnd_widget,
-        ))));
-        app.register_screen(String::from("game_dnd_setup"), game_dnd_conf)
-            .unwrap();
-        app.register_screen(String::from("game_dnd"), game_dnd_widget)
             .unwrap();
         let game: Box<dyn eframe::App> = Box::new(app);
         Ok(game)
